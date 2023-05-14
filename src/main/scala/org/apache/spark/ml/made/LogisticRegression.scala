422d542cbbb9ec58f2ffb6dbaa95f890a0b88e4b
andrey <rotanov07@mail.ru>
2023-05-14 16:25:51 +0300

    cleaning the code

--- src/main/scala/org/apache/spark/ml/made/LogisticRegression.scala
+++ src/main/scala/org/apache/spark/ml/made/LogisticRegression.scala
@@ -6,7 +6,10 @@ import org.apache.spark.ml.attribute.AttributeGroup
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.linalg.{DenseVector, Vector, VectorUDT, Vectors}
 import org.apache.spark.ml.param.{BooleanParam, Param, ParamMap}
-import org.apache.spark.ml.param.shared.{HasElasticNetParam, HasFeaturesCol, HasInputCol, HasLabelCol, HasMaxIter, HasOutputCol, HasPredictionCol, HasStepSize}
+import org.apache.spark.ml.param.shared.{
+  HasElasticNetParam, HasFeaturesCol, HasInputCol,
+  HasLabelCol, HasMaxIter, HasOutputCol, HasPredictionCol, HasStepSize
+}
 import org.apache.spark.ml.stat.Summarizer
 import org.apache.spark.ml.util._
 import org.apache.spark.ml.{Estimator, Model}
@@ -67,45 +70,18 @@ class LogisticRegression(override val uid: String) extends Estimator[LogisticReg
 
 
   override def fit(dataset: Dataset[_]): LogisticRegressionModel = {
-    //    val assembler = new VectorAssembler()
-    //      .setInputCols(Array("feature1", "feature2", "feature3"))
-    //      .setOutputCol("features")
-
-    //    val assembledData = assembler.transform(dataset).select("features", "label")
-    //    dataset.show(false)
-    //    val assembler: VectorAssembler = new VectorAssembler()
-    //      .setInputCols(Array($(featuresCol), "ones", $(labelCol)))
-    //      .setOutputCol("features_ext")
-    //
-    ////    val datasetExt: Dataset[_] = dataset.withColumn("ones", lit(1))
-    //    val assembledData = assembler.transform(datasetExt).select("features_ext")
-    //    assembledData.show(false)
-//    val features = dataset.columns.filterNot(_ == $(labelCol))
-//    val assembler = new VectorAssembler()
-//      .setInputCols(features)
-//      .setOutputCol("features")
-//
-//
-//    val assembledData = assembler.transform(dataset).select("features", "label")
-
     val assembledData = dataset.select(dataset($(featuresCol)), dataset($(labelCol)))
     val numFeatures: Int = MetadataUtils.getNumFeatures(assembledData, $(featuresCol))
-    //    val m = assembledData.first().getAs[Vector](0).size
-
 
     var theta = Vectors.dense(Array.fill(numFeatures + 1)(0.0))
-    //    var theta: BreezeDenseVector[Double] = BreezeDenseVector.rand[Double](numFeatures + 1)
     for (i <- 1 until $(maxIter)) {
       println(i)
       val grad = assembledData.rdd.map { row =>
         val label = row.getDouble(1)
         val features = row.getAs[Vector](0)
         val x = BreezeDenseVector.vertcat(new BreezeDenseVector(Array(1.0)), new BreezeDenseVector(features.toArray))
-//        val x = Vectors.dense(Array.concat(Array(1.0), features.toArray))
         val h = sigmoid(theta.asBreeze.dot(x))
         val error = h - label
-//        val h = sigmoid(theta.asBreeze.dot(x))
-        //        error * x.toDenseVector
         error * x
       }.reduce((a, b) => Vectors.dense(a.toArray.zip(b.toArray).map {
         case (x, y) => x + y
@@ -116,106 +92,10 @@ class LogisticRegression(override val uid: String) extends Estimator[LogisticReg
       theta = Vectors.dense(theta.toArray.zip(grad.toArray).map { case (x, y) => x - $(stepSize) * y })
     }
 
-
-    //    var theta: BreezeDenseVector[Double] = BreezeDenseVector.rand[Double](numFeatures + 1)
-    //    for (i <- 1 to $(maxIter)) {
-    //      val grad = assembledData.rdd.map { row =>
-    //          val label = row.getDouble(1)
-    //          val features = row.getAs[Vector](0)
-    //          val x = BreezeDenseVector.vertcat(new BreezeDenseVector(Array(1.0)), new BreezeDenseVector(features.toArray))
-    //          val h = sigmoid(theta.dot(x)) cv
-    //          val error = h - label
-    //          error * x
-    //      }.reduce(_ + _)
-    //
-    //      theta -= $(stepSize) * grad
-    //    }
-
-    //
-    //    for (i <- 1 to $(maxIter)) {
-    //      val grad = vectors.map { row =>
-    //        val label = row.getDouble(1)
-    //        val features = row.getAs[Vector](0)
-    //        //        val x = DenseVector.vertcat(new DenseVector(Array(1.0)), new DenseVector(features.toArray))
-    //        val x = Vectors.dense(Array.concat(Array(1.0), features.toArray))
-    //        val h = sigmoid(theta.dot(x))
-    //        val error = h - label
-    //        error * x
-    //      }.reduce((a, b) => Vectors.dense(a.toArray.zip(b.toArray).map { case (x, y) => x + y }))
-    //
-    //      theta -= $(stepSize) * grad
-    //    }
-    //    copyValues(new LogisticRegressionModel(Vectors.fromBreeze(theta(0 until theta.size - 1)).toDense))
-    //      .setParent(this)
-
     copyValues(new LogisticRegressionModel(theta.toDense))
       .setParent(this)
-    //
-    //    copyValues(new LogisticRegressionModel(Vectors.fromBreeze(theta(0 until theta.size - 1)).toDense))
-    //          .setParent(this)
+
   }
-
-
-  //  override def fit(dataset: Dataset[_]): LinearRegressionModel = {
-  //
-  //    // Used to convert untyped dataframes to datasets with vectors
-  ////    implicit val encoder: Encoder[Vector] = ExpressionEncoder()
-  //    val datasetExt: Dataset[_] = dataset.withColumn("ones", lit(1))
-  //    val numFeatures: Int = MetadataUtils.getNumFeatures(dataset, $(featuresCol))
-  //
-  //    val assembler: VectorAssembler = new VectorAssembler()
-  //      .setInputCols(Array($(featuresCol), "ones", $(labelCol)))
-  //      .setOutputCol("features_ext")
-  //
-  ////    val vectors: Dataset[Vector] = assembler.transform(datasetExt).select("features_ext").as[Vector]
-  //    val vectors = assembler.transform(datasetExt).select("features_ext")
-  //    var theta: BreezeDenseVector[Double] = BreezeDenseVector.rand[Double](numFeatures + 1)
-  //
-  //
-  //    for (i <- 1 to $(maxIter)) {
-  //      val grad = vectors.map { row =>
-  //        val label = row.getDouble(1)
-  //        val features = row.getAs[Vector](0)
-  ////        val x = DenseVector.vertcat(new DenseVector(Array(1.0)), new DenseVector(features.toArray))
-  //        val x = Vectors.dense(Array.concat(Array(1.0), features.toArray))
-  //        val h = sigmoid(theta.dot(x))
-  //        val error = h - label
-  //        error * x
-  //      }.reduce((a, b) => Vectors.dense(a.toArray.zip(b.toArray).map { case (x, y) => x + y }))
-  //
-  //      theta -= $(stepSize) * grad
-  //    }
-  //    copyValues(new LinearRegressionModel(Vectors.fromBreeze(theta(0 until theta.size - 1)).toDense))
-  //      .setParent(this)
-  //
-  ////
-  //    for (i <- 1 to $(maxIter)) {
-  //      val gradient = dataset.rdd.map(point => (point.label - predict(point.features, weights)) * point.features)
-  //        .reduce((a, b) => Vectors.asBreeze((0 until numFeatures).map(i => a(i) + b(i)).toArray))
-  //      weights = Vectors.dense((0 until numFeatures).map(i => weights(i) + stepSize * gradient(i)).toArray)
-  //    }
-  ////
-  ////    weights
-  ////
-  ////
-  ////    for (_ <- 0 until $(maxIter)) {
-  ////      val summary = vectors.rdd
-  ////        .mapPartitions((data: Iterator[Vector]) => {
-  ////          val summarizer = new MultivariateOnlineSummarizer()
-  ////          data.foreach(vector => {
-  ////            val X = vector.asBreeze(0 until w.size).toDenseVector
-  ////            val y = vector.asBreeze(w.size)
-  ////            summarizer.add(fromBreeze(X * (sum(X * w) - y)))
-  ////          })
-  ////          Iterator(summarizer)
-  ////        })
-  ////        .reduce(_ merge _)
-  ////      w = w - $(stepSize) * summary.mean.asBreeze
-  ////    }
-  //
-  ////    copyValues(new LinearRegressionModel(Vectors.fromBreeze(w(0 until w.size - 1)).toDense,w(w.size - 1)))
-  ////      .setParent(this)
-  //  }
 
   private def sigmoid(z: Double): Double = {
     1.0 / (1.0 + math.exp(-z))
@@ -266,32 +146,6 @@ class LogisticRegressionModel private[made](
 
     dataset.withColumn($(predictionCol), predictionUDF(dataset($(featuresCol))))
   }
-
-  //  override def transform(dataset: Dataset[_]): DataFrame = {
-  //    val sigmoidUdf = udf((z: Double) => 1.0 / (1.0 + math.exp(-z)))
-  //
-  ////    val predictUdf = udf((features: Vector) => {
-  ////        val x = Vectors.dense(Array.concat(Array(1.0), features.toArray))
-  ////        sigmoidUdf(theta.dot(x))
-  ////    })
-  //
-  //    val predictUdf = dataset.sqlContext.udf.register(uid + "_transform",
-  //      (x: Vector) => {
-  //        sigmoidUdf((theta.asBreeze dot x.asBreeze).toDouble))
-  //      })
-  //
-  ////    dataset.withColumn("prediction", predictUdf($"features"))
-  //    dataset.withColumn($(predictionCol), predictUdf(dataset($(featuresCol))))
-
-  //    val transformUdf =
-  //      dataset.sqlContext.udf.register(uid + "_transform",
-  //        (x: Vector) => {
-  //          Vectors.fromBreeze(BreezeDenseVector(w.asBreeze.dot(x.asBreeze) + b))
-  //        }
-  //      )
-  //
-  //    dataset.withColumn($(predictionCol), transformUdf(dataset($(featuresCol))))
-  //  }
 
   override def transformSchema(schema: StructType): StructType = validateAndTransformSchema(schema)
 
--- src/test/scala/org/apache/spark/ml/made/LogisticRegressionTest.scala
+++ src/test/scala/org/apache/spark/ml/made/LogisticRegressionTest.scala
@@ -1,26 +1,5 @@
-//package org.apache.spark.ml.made
-//
-//import breeze.linalg.{*, DenseMatrix, DenseVector}
-////import breeze.linalg.{sum, DenseMatrix => BreezeDenseMatrix, DenseVector => BreezeDenseVector}
-//import breeze.optimize.DiffFunction.castOps
-//import org.apache.spark.ml.linalg.{DenseVector, Vector, VectorUDT, Vectors}
-//import com.google.common.io.Files
-//import org.apache.spark.ml.feature.VectorAssembler
-//import org.apache.spark.ml.linalg.Vectors
-//import org.apache.spark.ml.made.LinearRegressionTest.sqlc
-//import org.apache.spark.ml.util.Identifiable
-//import org.apache.spark.ml.{Pipeline, PipelineModel}
-//import org.apache.spark.sql.DataFrame
-//import org.apache.spark.sql.functions.udf
-//import org.scalatest._
-//import org.scalatest.flatspec._
-//import org.scalatest.matchers._
-
-
 package org.apache.spark.ml.made
 
-//import org.apache.spark.implicits._
-//import sqlc.implicits._
 
 import org.apache.spark.sql.functions.udf
 import com.google.common.io.Files
@@ -42,21 +21,11 @@ class LogisticRegressionTest extends AnyFlatSpec with should.Matchers with WithS
 
   val delta = 0.01
   val expected_theta: DenseVector[Double] = LogisticRegressionTest._theta
-  //  val b: Double = LinearRegressionTest._b
-  //  val expected_labels: DenseVector[Double] = LogisticRegressionTest._labels
   val data: DataFrame = LogisticRegressionTest._data
 
-  //  System.setProperty("hadoop.home.dir","C:\\Users\\rotan\\hadoop" );
-  //  var path = scala.util.Properties.envOrElse("HADOOP_HOME", "undefined")
-  //  println(s"----------------------------------------------------------------- $path")
-  //  System.setProperty("hadoop.home.dir", scala.util.Properties.envOrElse("HADOOP_HOME", "undefined"));
   private def validateModel(model: LogisticRegressionModel): Unit = {
     model.theta.size should be(expected_theta.size)
-//    model.theta(0) should be(expected_theta(0) +- delta)
-//    model.theta(1) should be(expected_theta(1) +- delta)
-//    model.theta(2) should be(expected_theta(2) +- delta)
 
-    //    model.b should be(b +- delta)
   }
 
   private def validateModelAndData(model: LogisticRegressionModel, data: DataFrame): Unit = {
@@ -136,15 +105,8 @@ object LogisticRegressionTest extends WithSpark {
   val DATA_SIZE = 30000
   val VECTOR_SIZE = 3
 
-  //  lazy val _data: DataFrame = createDataFrame(_X, _labels)
-  //  lazy val _data: DataFrame = transform_data(_X)
-  //  lazy val _X: DenseMatrix[Double] = DenseMatrix.rand[Double](DATA_SIZE, VECTOR_SIZE)
   lazy val _theta: DenseVector[Double] = DenseVector(1.0, 1.5, 0.3, -0.7)
-  //  val _labels = _theta.dot(_X)
-  //  val predictUdf = _data.sqlContext.udf.register(Identifiable.randomUID("_transform"),
-  //    (x: Vector) => {
-  //      (_theta.asBreeze dot x.asBreeze)
-  //    })
+
   lazy val _features = Seq.fill(30000)(
     Vectors.fromBreeze(DenseVector.rand(3))
   )
@@ -154,26 +116,6 @@ object LogisticRegressionTest extends WithSpark {
     _features.map(x => Tuple1(x)).toDF("features")
   }
 
-  //  def transform_data(_x: DenseMatrix[Double]): DataFrame = {
-  //    lazy val generated_data = _x(*, ::).iterator
-  //      .map(x => (x(0), x(1), x(2)))
-  //      .toSeq
-  //      .toDF("x1", "x2", "x3")
-
-  //    val assembler = new VectorAssembler()
-  //      .setInputCols(Array("x1", "x2", "x3"))
-  //      .setOutputCol("features")
-
-  //    generated_data.show(false)
-  //    println(generated_data("x1"))
-  //    assembledData.show(false)
-  //
-  //    generated_data.show(false)
-  //    val assembledData = assembler.transform(generated_data)
-  //      .select("features")
-  //      .rdd
-  //      .map(row => row.getAs[Vector]("features"))
-
   val predictionUDF = udf((features: Vector) => {
     val x = Vectors.dense(Array.concat(Array(1.0), features.toArray))
     val dotProduct = _theta dot x.asBreeze
@@ -181,181 +123,11 @@ object LogisticRegressionTest extends WithSpark {
     prediction
   })
 
-  import sqlc.implicits._
-
-  // Сырые данные
-  //    val assembledData = assembler.transform(generated_data).select("features")
-//  lazy val tmp_data = _features.map(x => (x(0), x(1), x(2))).toDF("x1", "x2", "x3")
-//  lazy val gg = generated_data.withColumn("label", predictionUDF(generated_data("features"))).drop("features")
-//  val _data = tmp_data.join(gg)
 
   lazy val _data =  generated_data.withColumn("label", predictionUDF(generated_data("features")))
 
-//  _data.show()
-
-  //
-////  val merged_df = tmp_data.unionByName(gg("label"))
-//  tmp_data.show(false)
-//  import org.apache.spark.sql.functions._
-////  println(gg.select("label").map(r => r(0).asInstanceOf[Double]).collect())
-//  lazy val _data = tmp_data.withColumn("label", gg("label"))
-////  _data.show(false)
-////  lazy val _data =
-//  println(8)
-  //    println(assembledData.select("features")("features"))
-  //    val schemaSeq = Seq("empno", "ename", "designation", "manager")
-  //
-  //    //Create Empty DataFrame using Seq
-  //    val emptyDF3 = Seq.empty[(String, String, String, String)].toDF(schemaSeq: _*)
-  //    assembledData.select()
-  //    val tt = predictionUDF(assembledData("features"))
-  //    println(tt)
-  //    assembledData.explode(assembledData("features"))
-  //    emptyDF3.withColumn("labels", predictionUDF(assembledData("features")))
-  //    generated_data.withColumn("labels", predictionUDF(assembledData("features"))).show(false)
-
-  //  lazy val _data =  generated_data.withColumn("label", predictionUDF(generated_data("features")))
-
-  //    generated_data.withColumn("labels", assembledData
-  //      .map(row => predictionUDF(row("features"))).toDF() )
-  ////    generated_data.show(false)
-  //    generated_data
-  //  }
 
   private def sigmoid(z: Double): Double = {
     1.0 / (1.0 + math.exp(-z))
   }
 }
-//    assembler.transform(generated_data)
-//      .select("prediction")
-//      .rdd
-//      .map(row => row.getAs[DenseVector]("prediction"))
-//
-//  def createDataFrame(x: DenseMatrix[Double], y: DenseVector[Double]): DataFrame = {
-//    lazy val data: DenseMatrix[Double] =
-//      DenseMatrix.horzcat(x, y.asDenseMatrix.t)
-//
-//    lazy val generated_data = data(*, ::).iterator
-//      .map(x => (x(0), x(1), x(2), x(3)))
-//      .toSeq
-//      .toDF("x1", "x2", "x3", "label")
-//
-//    lazy val assembler = new VectorAssembler().setInputCols(Array("x1", "x2", "x3")).setOutputCol("features")
-//
-//    lazy val _data: DataFrame = assembler.transform(generated_data).select("features", "label")
-//
-//    _data
-//  }
-//
-
-//  val sigmoidUdf = udf((z: Double) => 1.0 / (1.0 + math.exp(-z)))
-//  lazy val _features = Seq.fill(100000)(
-//    BreezeDenseVector.rand(3)
-//  )
-//  //  lazy val _features = DenseMatrix.rand[Double](DATA_SIZE, VECTOR_SIZE)
-//  //  lazy val _theta =  Vectors.dense(1.5, 0.3, -0.7).asBreeze
-//  lazy val _theta = BreezeDenseVector(1.5, 0.3, -0.7)
-//  //  lazy val _labels =  sigmoidUdf((_theta.dot(_features)))
-//  //  lazy val _data: DataFrame = createDataFrame(_features, _labels)
-//  //  lazy val _b: Double = 1.0 bnm
-//
-//  lazy val _data: DataFrame = {
-//    import sqlc.implicits._
-//    _features.map(x => Tuple1(x)).toDF("features")
-//  }
-//
-//  lazy val transformUdf =
-//    udf((x: Vector) => {
-//      _theta dot x.asBreeze
-//    })
-//
-//  //  val data = _data.withColumn("features", transformUdf(_data("features")))
-//
-//
-//  val predictUdf = _data.sqlContext.udf.register(Identifiable.randomUID("_transform"),
-//    (x: Vector) => {
-//      (_theta dot x.asBreeze)
-//    })
-//
-//  lazy val _labels = predictUdf(_data("label"))
-
-//  def createDataFrame(x: DenseMatrix[Double], y: DenseVector[Double]): DataFrame = {
-//    import sqlc.implicits._
-//    lazy val data: DenseMatrix[Double] =
-//      DenseMatrix.horzcat(x, y.asDenseMatrix.t)
-//
-//    lazy val generated_data = data(*, ::).iterator
-//      .map(x => (x(0), x(1), x(2), x(3)))
-//      .toSeq
-//      .toDF("x1", "x2", "x3", "label")
-//
-//    lazy val assembler = new VectorAssembler().setInputCols(Array("x1", "x2", "x3")).setOutputCol("features")
-//
-//    lazy val transformUdf =
-//      udf((x: Vector) => {
-//        _theta dot x.asBreeze
-//      })
-//    lazy val _data = _data.withColumn("target", transformUdf(_data("features")))
-//    lazy val _data: DataFrame = assembler.transform(generated_data).select("features", "label")
-//
-//    _data
-//  }
-
-
-//
-//object LinearRegressionTest extends WithSpark {
-//  val DATA_SIZE = 30000
-//  val VECTOR_SIZE = 3
-//  val sigmoidUdf = udf((z: Double) => 1.0 / (1.0 + math.exp(-z)))
-//  lazy val _features = Seq.fill(100000)(
-//    BreezeDenseVector.rand(3)
-//  )
-//  //  lazy val _features = DenseMatrix.rand[Double](DATA_SIZE, VECTOR_SIZE)
-//  //  lazy val _theta =  Vectors.dense(1.5, 0.3, -0.7).asBreeze
-//  lazy val _theta = BreezeDenseVector(1.5, 0.3, -0.7)
-//  //  lazy val _labels =  sigmoidUdf((_theta.dot(_features)))
-//  //  lazy val _data: DataFrame = createDataFrame(_features, _labels)
-//  //  lazy val _b: Double = 1.0 bnm
-//
-//  lazy val _data: DataFrame = {
-//    import sqlc.implicits._
-//    _features.map(x => Tuple1(x)).toDF("features")
-//  }
-//
-//  lazy val transformUdf =
-//    udf((x: Vector) => {
-//      _theta dot x.asBreeze
-//    })
-//
-////  val data = _data.withColumn("features", transformUdf(_data("features")))
-//
-//
-//  val predictUdf = _data.sqlContext.udf.register(Identifiable.randomUID("_transform"),
-//    (x: Vector) => {
-//      (_theta dot x.asBreeze)
-//    })
-//
-//  lazy val _labels = predictUdf(_data("label"))
-//
-//  //  def createDataFrame(x: DenseMatrix[Double], y: DenseVector[Double]): DataFrame = {
-//  //    import sqlc.implicits._
-//  //    lazy val data: DenseMatrix[Double] =
-//  //      DenseMatrix.horzcat(x, y.asDenseMatrix.t)
-//  //
-//  //    lazy val generated_data = data(*, ::).iterator
-//  //      .map(x => (x(0), x(1), x(2), x(3)))
-//  //      .toSeq
-//  //      .toDF("x1", "x2", "x3", "label")
-//  //
-//  //    lazy val assembler = new VectorAssembler().setInputCols(Array("x1", "x2", "x3")).setOutputCol("features")
-//  //
-//  //    lazy val transformUdf =
-//  //      udf((x: Vector) => {
-//  //        _theta dot x.asBreeze
-//  //      })
-//  //    lazy val _data = _data.withColumn("target", transformUdf(_data("features")))
-//  //    lazy val _data: DataFrame = assembler.transform(generated_data).select("features", "label")
-//  //
-//  //    _data
-//  //  }
-//}
